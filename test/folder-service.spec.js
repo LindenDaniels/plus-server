709fd1c2a9cdab3c6cf97ca0873423bc550faa6c
Linden Daniels <lindendaniels@gmail.com>
2019-12-01 20:43:14 -0500

    Fix testing wording

--- test/folder-service.spec.js
+++ test/folder-service.spec.js
@@ -71,7 +71,7 @@ describe('Folder Endpoints', function() {
         const folderId = 123456
         return supertest(app)
           .get(`/api/folders/${folderId}`)
-          .expect(404, { error: `Folder doesn't exist` })
+          .expect(404, { error: { message: 'Folder Not Found' }
       })
     })
 
