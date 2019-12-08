5986a4d86cf495e3c16a5aae2673ea9c20d09b91
Linden Daniels <lindendaniels@gmail.com>
2019-12-08 10:50:23 -0500

    Remove changes from troubleshooting

--- src/server.js
+++ src/server.js
@@ -6,19 +6,9 @@ const { PORT, DATABASE_URL, API_BASE_URL  } = require('./config')
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  "pool": {
-    "min": 2,
-     max: 50,
-    "createTimeoutMillis": 3000,
-    "acquireTimeoutMillis": 30000,
-    "idleTimeoutMillis": 30000,
-    "reapIntervalMillis": 1000,
-    "createRetryIntervalMillis": 100,
-    "propagateCreateError": false // <- default is true, set to false
-  },
 })
 
-console.log(db);
+
 
 const cors = require('cors');
 const {CLIENT_ORIGIN} = require('./config');
